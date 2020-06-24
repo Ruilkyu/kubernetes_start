70299b1eb9422dadfdcaf455956c0ea52fac2831
lurui <lurui@ipfsforce.com>
2020-06-24 22:16:59 +0800

    调整master_start步骤优先生成masters_hosts

--- master_start.py
+++ master_start.py
@@ -2,6 +2,10 @@
 时间：2020/6/17
 作者：lurui
 功能：master统一入口
+
+时间：2020/6/24
+作者：lurui
+功能：master各模块启动顺序，优先生成master_hosts
 """
 
 # -*- coding: utf-8 -*-
@@ -21,15 +25,16 @@ from runs.kubernetes.start_master import start_master
 
 
 def start():
+    start_ansible()
+    start_ansible_hosts()
+    start_cert()
+    gencerts('master')
+
     start_haproxy_cfg()
     start_haproxy()
     start_keepalived_cfg()
     start_keepalived()
 
-    start_ansible()
-    start_ansible_hosts()
-    start_cert()
-    gencerts('master')
     initenv('master')
     copycerts('master')
     start_master()
